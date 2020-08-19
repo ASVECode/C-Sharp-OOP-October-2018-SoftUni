7a6e19be3d47a944211cbb24a0f9d1f6ff05d7dc
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-08-19 17:00:51 +0300

    add defining classes

---
+++ 01.Defining Classes/01. Bank Account/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Bank_Account
+{
+    class BankAccount
+    {
+        private int id;
+        private decimal balance;
+
+        public BankAccount()
+        {
+
+        }
+
+        public BankAccount(int id)
+        {
+            this.id = id;
+        }
+
+        public BankAccount(int id, decimal balance) : this(id)
+        {
+            this.balance = balance;
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return this.balance;
+            }
+
+            set
+            {
+                this.balance = value;
+            }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return amount;
+            }
+            throw new ArgumentException("Insufficient amount!");
+        }
+
+        public override string ToString()
+        {
+            return $"Your balance is {balance}";
+        }
+
+    }
+}
---
+++ 01.Defining Classes/01. Bank Account/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(1, 100);
+            Console.WriteLine(bankAccount.Balance);
+
+            bankAccount.Deposit(900);
+            bankAccount.Withdraw(50);
+            Console.WriteLine(bankAccount.ToString());
+        }
+    }
+}

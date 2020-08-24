f28ce08a08469ea06193df082b2b4357912f1a73
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-08-24 19:45:35 +0300

    add defining classes

---
+++ 01.Defining Classes/07. Company Roster/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CompanyRoster
+{
+    public class Employee
+    {
+        private string name;
+        private double salary;
+        private string position;
+        private string department;
+        private string email;
+        private int age;
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+        public double Salary
+        {
+            get { return this.salary; }
+            set { this.salary = value; }
+        }
+        public string Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+        public string Department
+        {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+
+        public Employee(string name, double salary, string position, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Age = -1;
+            this.Email = "n/a";
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Salary:F2} {Email} {Age}";
+        }
+
+    }
+}
